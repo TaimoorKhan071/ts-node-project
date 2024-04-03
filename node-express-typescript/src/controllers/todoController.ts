e7b359c2efc744ec1af7e7783ccb0334d0dddef1
Taimoor Khan <taimoorkhan@rapidzzsolutions.com>
2024-04-03 16:09:14 +0500

    feature/update todo controller

--- node-express-typescript/src/controllers/todoController.ts
+++ node-express-typescript/src/controllers/todoController.ts
@@ -22,3 +22,32 @@ export const getTodoById = async (req: Request, res: Response) => {
     console.log(err);
   }
 };
+
+export const updateTodo = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const updates = req.body;
+
+    // Update the todo item with the provided data
+    const todoItem = await todo.findById(id);
+    if (!todoItem) {
+      throw new Error("Todo Item not found");
+    }
+
+    // Update the todo item with the provided data
+    todoItem.text = updates.text;
+    await todoItem.save();
+
+    res.json({
+      updated: true,
+      todo: todoItem,
+    });
+  } catch (err: any) {
+    console.log(err);
+    res.json({
+      updated: false,
+      todo: null,
+      error: err.message,
+    });
+  }
+};
--- node-express-typescript/src/routes/todo.routes.ts
+++ node-express-typescript/src/routes/todo.routes.ts
@@ -1,14 +1,13 @@
 import express from "express";
-import { getTodoById, getTodos } from "../controllers/todoController";
+import { getTodoById, getTodos, updateTodo } from "../controllers/todoController";
 
 const todoRouter = express.Router();
 
 todoRouter.get("/api/v1/todos/:id", getTodoById);
 todoRouter.get("/api/v1/todos", getTodos);
+todoRouter.put("/api/v1/todos/update/:id", updateTodo);
 
-export default todoRouter 
-
+export default todoRouter;
 
 // todoRouter.post(    "/api/v1/todos/add");
-// todoRouter.put(     "/api/v1/todos/update/:id");
 // todoRouter.delete(  "/api/v1/todos/delete/:id");
