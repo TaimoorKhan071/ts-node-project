1ec4b233bf81573e7e4c780a3e35e3b88b38a4b9
Taimoor Khan <taimoorkhan@rapidzzsolutions.com>
2024-04-03 14:17:26 +0500

    App initialization complete

---
+++ node-express-typescript/src/index.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectMongoDB } from "./config/db";
+
+dotenv.config();
+
+const app: Application = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+// app.use(todoRouter)
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Express & TypeScript Server");
+});
+
+const initServer = async () => {
+  try {
+    await connectMongoDB();
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+};
+
+initServer();
